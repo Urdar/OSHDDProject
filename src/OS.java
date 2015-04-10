be39ad10a0f625cbb54baf305ae3854df722bb11
vartdal <vartdal86@gmail.com>
2015-04-10 10:14:43 +0200

    :)

---
+++ src/OS.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Jarlè on 10.04.2015.
+ */
+public class OS {
+
+
+        public void write(String input) {
+
+        }
+
+        public String read(int index) {
+
+
+
+        }
+
+        public int seek(String searchString) {
+
+        }
+
+    }
+
+
+}
