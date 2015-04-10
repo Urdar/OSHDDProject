46fd4a83bbafc3f0b67f129ac8809ed9ce1ad907
tyfonknut <tyfonknut@gmail.com>
2015-04-10 10:17:21 +0200

    Lagt til funksjoner for driver klassen

--- src/Driver.java
+++ src/Driver.java
@@ -2,4 +2,23 @@
  * Created by knut on 10.04.15.
  */
 public class Driver {
+
+    HDD hardDrive;
+
+    public void dDriver(HDD hardDrive){
+        this.hardDrive = hardDrive;
+    }
+
+    public String dRead(int index){
+        return hardDrive.read(index);
+    }
+
+    public void dWrite(String input){
+        hardDrive.write(input);
+    }
+
+    public int dSearch(String searchString){
+        return hardDrive.seek(searchString);
+    }
+
 }
